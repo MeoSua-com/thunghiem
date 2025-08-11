cdd181a08d6c6cfcde964b860a5b9bb72c8ac759
akagiyuu <khanghuynh0245@gmail.com>
2025-08-11 17:49:12 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,70 @@
-use std::time::Duration;
-
-use clap::Parser;
+use std::{env, time::Duration};
 
 use anyhow::Result;
-use thirtyfour::prelude::*;
+use chromiumoxide::{
+    Page,
+    browser::{Browser, BrowserConfig},
+    layout::Point,
+};
+use futures::StreamExt;
 use tokio::time::sleep;
 
-async fn click_ad(port: u16, wait_time: u64) -> Result<()> {
-    let mut caps = DesiredCapabilities::chrome();
-    caps.add_arg("--window-size=1920,1080")?; // Optional: set window size for visual consistency
-    let driver = WebDriver::new(format!("http://localhost:{port}"), caps).await?;
+const WIDTH: u32 = 300;
+const HEIGHT: u32 = 300;
+const AD_X: f64 = 506.;
+const AD_Y: f64 = 27.;
+const AD_COUNT: usize = 4;
+const WAIT_TIME: Duration = Duration::from_secs(3);
 
-    driver.goto("https://meosua.com/").await?;
+async fn click(page: &Page, browser: &Browser) -> Result<()> {
+    let point = Point::new(AD_X, AD_Y);
+    let mut current_tab_count = browser.pages().await?.len();
+    let targeted_tab_count = current_tab_count + AD_COUNT;
 
-    let ad = driver
-        .query(By::Css("html > div:nth-child(4) > iframe"))
-        .wait(Duration::from_secs(120), Duration::from_millis(100))
-        .first()
-        .await?;
+    while current_tab_count < targeted_tab_count {
+        let mut new_tab_count = current_tab_count;
 
-    ad.enter_frame().await?;
-    driver
-        .find(By::Css("#container > div:nth-child(3) > button"))
-        .await?
-        .click()
-        .await?;
-    sleep(Duration::from_secs(wait_time)).await;
+        while new_tab_count == current_tab_count {
+            page.click(point).await?;
+            new_tab_count = browser.pages().await?.len();
+            sleep(Duration::from_millis(100)).await;
+        }
+        current_tab_count = new_tab_count;
 
-    driver.quit().await?;
+        println!("Task left: {}", targeted_tab_count - current_tab_count);
+        sleep(WAIT_TIME).await;
+
+        page.bring_to_front().await?;
+    }
 
     Ok(())
 }
 
-#[derive(Debug, Parser)]
-#[command(version, about, long_about = None)]
-struct Cli {
-    /// WebDriver port
-    #[arg(short, long, default_value_t = 9515)]
-    port: u16,
-
-    /// Time to wait in second before closing the browser
-    #[arg(short, long, default_value_t = 5)]
-    wait_time: u64,
-}
-
 #[tokio::main]
 async fn main() -> Result<()> {
-    let cli = Cli::parse();
+    let (mut browser, mut handler) = Browser::launch(
+        BrowserConfig::builder()
+            .window_size(WIDTH, HEIGHT)
+            .no_sandbox()
+            .build()
+            .unwrap(),
+    )
+    .await?;
+    tokio::task::spawn(async move {
+        loop {
+            let _ = handler.next().await;
+        }
+    });
 
-    click_ad(cli.port, cli.wait_time).await?;
+    let url = env::var("URL").unwrap();
 
-    println!("Succeed");
+    let page = browser.new_page("about:blank").await?;
+    page.enable_stealth_mode().await?;
+    page.goto(url).await?;
+
+    click(&page, &browser).await?;
+
+    browser.close().await?;
 
     Ok(())
 }
